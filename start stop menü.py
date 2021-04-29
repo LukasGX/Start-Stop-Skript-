4665daa070a32bba885ac329ff3e26e14223e0d5
JonPlaytv <83393169+JonPlaytv@users.noreply.github.com>
2021-04-29 09:20:40 +0200

    Add files via upload

---
+++ start stop menü.py
@@ -0,0 +1,15 @@
+
+
+print("==============================================================\n")
+print("Select option: \n")
+print("1)Start\n")
+print("2)Exit\n")
+user_input = str(input("Your option: "))
+if user_input == "1":
+ #program
+
+
+elif user_input == "2":
+    exit()
+else:
+    print('Unknown input!\n')
