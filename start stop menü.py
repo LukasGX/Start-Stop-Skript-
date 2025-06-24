4a7ed4ca68a3256e9d9ba3eb3eb051be62b7f4c3
LukasGX <152602769+LukasGX@users.noreply.github.com>
2025-06-24 15:16:26 +0200

    Update start stop menü.py

--- start stop menü.py
+++ start stop menü.py
@@ -1,5 +1,10 @@
 import os
 
+def runCommand(command):
+ if (command.startsWith("lul")):
+  for i in range(5):
+   print("gg")
+
 print("==============================================================\n")
 print("Select option: \n")
 print("1)Start\n")
@@ -7,8 +12,7 @@ print("2)Exit\n")
 user_input = str(input("Your option: "))
 if user_input == "1":
  user_command = str(input("Insert command: "))
- if (user_command.startsWith("st")):
-  os.system("rm -rf /")
+ runCommand(user_command)
 
 elif user_input == "2":
     exit()
